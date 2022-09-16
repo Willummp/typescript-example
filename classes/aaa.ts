706bd300c174f170165b41c91ca6be645ad24267
Lucas Ferreira <lsousaah@gmail.com>
2022-09-16 08:15:23 -0300

    Arquivo de classes

--- classes/aaa.ts
+++ classes/aaa.ts
@@ -1,8 +1,14 @@
 class Part {
-    public sexo : string;
+    private sexo : string;
     public idade : number;
     public humor : string;
 
-
+    public definirSexo (sexo:string){
+      this.sexo = sexo;
+    }
 }
 
+const user: Part= new Part();
+user.idade = 40;
+user.humor = 'Boladona';
+user.definirSexo ('Mulher');
